9a5a9e9b98e8d95cd8c5513d92feecd595ea5420
jan diaz <jancarlosdiaz26@gmail.com>
2022-09-28 17:44:18 -0500

    se realizo nuevamente el modelo

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,4 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :game
-  belongs_to :supplier
 end
--- db/migrate/20220928224225_create_orders.rb
+++ db/migrate/20220928224225_create_orders.rb
@@ -2,10 +2,9 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
       t.date :date
-      t.integer :days
+      t.integer :day
       t.references :user, null: false, foreign_key: true
       t.references :game, null: false, foreign_key: true
-      t.references :supplier, null: false, foreign_key: true
 
       t.timestamps
     end
