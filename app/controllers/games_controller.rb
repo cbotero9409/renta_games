35ce8503cbc2a547ec996c8e0fee2b1a25bba4ff
dilbani <claudiaenriquez241@gmail.com>
2022-09-28 11:47:46 -0500

    Add controller the games and accions

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,32 @@
+class GamesController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  private
+
+  def game_params
+
+  end
+end
