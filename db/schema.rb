8cdf80551f65d378d4c604ec099d91ccf4545f5b
Camilo Botero <cbotero2709@gmail.com>
2022-09-28 19:11:42 -0500

    navbar and alerts added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_28_221959) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_233139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_28_221959) do
     t.boolean "available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.date "date"
+    t.integer "days"
+    t.bigint "user_id", null: false
+    t.bigint "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_orders_on_game_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_28_221959) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "games"
+  add_foreign_key "orders", "users"
 end
