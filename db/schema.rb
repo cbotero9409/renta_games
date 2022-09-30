82c839accb623ed56079e7c839967760603f0949
Camilo Botero <cbotero2709@gmail.com>
2022-09-30 10:46:24 -0500

    schema migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_29_005504) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,17 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_29_005504) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_games_on_user_id"
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.date "date"
-    t.integer "days"
-    t.bigint "user_id", null: false
-    t.bigint "game_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["game_id"], name: "index_orders_on_game_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -63,8 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_29_005504) do
   end
 
   add_foreign_key "games", "users"
-
   add_foreign_key "orders", "games"
   add_foreign_key "orders", "users"
-
 end
