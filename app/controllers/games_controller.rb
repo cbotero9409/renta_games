270a1cd74b92798b41ce80d272a396356012838a
dilbani <claudiaenriquez241@gmail.com>
2022-09-30 11:53:06 -0500

    index

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
 
   def index
-
+    @games = Game.all
   end
 
   def new
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_09_29_002337) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,10 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_29_002337) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "games", "users"
-
   add_foreign_key "orders", "games"
   add_foreign_key "orders", "users"
-
 end
