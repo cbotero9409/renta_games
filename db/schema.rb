ee9ad5f451e5e9a6df87af72e06b31afd1c02599
Camilo Botero <cbotero2709@gmail.com>
2022-10-01 13:46:00 -0500

    deleted date column from orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_01_003530) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_01_184338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,7 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_01_003530) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.date "date"
     t.integer "days"
     t.bigint "user_id", null: false
     t.bigint "game_id", null: false
