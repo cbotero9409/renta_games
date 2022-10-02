fdb47573820640efe60645cd89748f7dd66adb4e
Camilo Botero <cbotero2709@gmail.com>
2022-10-02 16:37:52 -0500

    corrected migration

--- db/migrate/20221001002505_change_column_in_games.rb
+++ db/migrate/20221001002505_change_column_in_games.rb
@@ -1,7 +1,5 @@
 class ChangeColumnInGames < ActiveRecord::Migration[7.0]
   def change
     change_column :games, :available, :boolean, default: true
-    remove_column :games, :photo
-    remove_column :users, :photo
   end
 end
