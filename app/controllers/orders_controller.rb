31bd4550e9288dfc506b4cb91018a2199b107d06
Camilo Botero <cbotero2709@gmail.com>
2022-10-02 15:10:49 -0500

    supplier orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,20 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.all.order(created_at: :desc)
+    if current_user.role == "provider"
+      @orders = supplier_orders
+    else
+      @orders = Order.all.order(created_at: :desc)
+    end
+  end
+
+  private
+
+  def supplier_orders
+    games = Game.where(user: current_user)
+    @orders = []
+    games.each do |game|
+      game.orders.each { |order| @orders << order }
+    end
+    return @orders
   end
 end
